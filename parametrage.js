640b6200b57b51cdaa958fddd5bcc6ff2c5dfee4
Deborah Fache <deb52.etu@orange.fr>
2020-06-26 01:57:48 +0200

    premier commit

---
+++ parametrage.js
@@ -0,0 +1,109 @@
+function validerMinuteur () {
+
+    let minuteur = document.getElementById("minuteur");
+
+    let heure = document.querySelector("#minuteur #heure");
+    let minute = document.querySelector("#minuteur #minute");
+    let seconde = document.querySelector("#minuteur #seconde");
+
+
+    heure.value = verif(heure.value, "heure");
+    minute.value = verif(minute.value, "minute");
+    seconde.value = verif(seconde.value, "seconde");
+
+
+    let minuteurElmt = document.createElement("p");
+    minuteurElmt.id = "affichageMinuteur";
+    minuteurElmt.textContent += `${heure.value}:${minute.value}:${seconde.value}`;
+
+    if (existe("affichageMinuteur")) {
+        minuteur.replaceChild(minuteurElmt, document.getElementById("affichageMinuteur"));
+    } else {
+        minuteur.appendChild(minuteurElmt);
+    }
+}
+
+function existe (id) {
+    return document.getElementById(id) !== null;
+}
+
+function verif (n, type) {
+    /*
+    Permet de mettre en forme et de vérifier les entrées nombre, et de renvoyer la valeur avec les éventuels changements
+     */
+    n = parseInt(n); // Passe les textes des champs en nombre
+    if (n < 0 || isNaN(n)) { // isNan car si pas un nombre le parseInt renvoi Nan
+        n = 0;
+    }
+    let limite;
+    switch (type) {
+        case "nombre":
+            limite = 50;
+            break;
+        case "heure":
+            limite = 12;
+            break;
+        case "minute":
+            limite = 59;
+            break;
+        case "seconde":
+            limite = 59;
+            break;
+        default:
+            console.error(`Erreur dans la vérification de la valeur ${n} de type ${type}`);
+            n = 0;
+    }
+    if (n > limite) {
+        n = limite;
+    }
+
+    return n;
+
+}
+
+function resetMinuteur() {
+    let minuteur = document.getElementById("minuteur");
+
+    let heure = document.querySelector("#minuteur #heure");
+    let minute = document.querySelector("#minuteur #minute");
+    let seconde = document.querySelector("#minuteur #seconde");
+
+    resetUnit(heure);
+    resetUnit(minute);
+    resetUnit(seconde);
+
+    if (existe("affichageMinuteur"))
+        minuteur.removeChild(document.getElementById("affichageMinuteur"));
+}
+
+function resetUnit (elmt) {
+    elmt.value = 0;
+}
+
+// let boutonMinuteur = document.getElementById("boutonMinuteurValider");
+// boutonMinuteur.addEventListener("click", minuteur);
+
+// let boutonResetMinuteur = document.getElementById("boutonMinuteurReset");
+// boutonResetMinuteur.addEventListener("click", resetMinuteur);
+
+
+// Annuler l'envoi automatique des formulaire sur une autre page
+let forms = document.getElementsByTagName("form");
+for (let f of forms) {
+    f.addEventListener("submit", function (e) {
+        e.preventDefault();
+    });
+}
+
+
+// TODO: fonctionne presque mais à voir plus tard comment ca va se passer avec l'affichage (besoin en nb ou ok en texte ?)
+// let champs = document.querySelectorAll("#minuteur input");
+// for (let ch of champs) {
+//     ch.addEventListener("blur", function () {
+//         if (ch.value.length === 1) {
+//             ch.value = "0" + ch.value;
+//         }
+//     });
+// }
+
+// TODO: suite du cours pour pouvoir afficher le chrono
